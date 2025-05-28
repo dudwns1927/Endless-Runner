87748782a5f9a821853994a1d89d7ac746e0706b
jaeyoon <dudwns1927@gmail.com>
2025-05-28 19:45:10 +0900

    (2025.5.28) 일반화 싱글톤

--- Game2/Assets/Scripts/SpeedManager.cs
+++ Game2/Assets/Scripts/SpeedManager.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedManager : MonoBehaviour
+public class SpeedManager : Singleton<SpeedManager>
 {
 
     static SpeedManager instance;
 
     [SerializeField] float speed = 30.0f;
+    [SerializeField] float limitSpeed = 60.0f;
 
     public float Speed { get { return speed; } }
-    public static SpeedManager Instance { get { return instance; } }
 
+    private void Start() {
+        StartCoroutine(Increase());
+    
+    }
 
-
-    private void Awake() {
-        if (instance == null) {
-            instance = this;
+    IEnumerator Increase() {
+        while (speed < limitSpeed) {
+            yield return CoroutineCache.WaitForSeconds(5.0f);
+            speed = speed + 2.5f;
         }
     }
 }
